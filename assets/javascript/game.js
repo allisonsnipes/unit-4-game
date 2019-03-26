95f4437d2677da02c46a461dabc9237d73fcba64
Allison Snipes <ajsnipes@usfca.edu>
2019-03-26 14:16:49 -0700

    working on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+'use strict'
+
+$(document).ready(function() {
+  let total,
+      wins,
+      losses = 0;
+  let randomNumber =
+});
+
+
+// special button functions to generate random number
+$(".button1").on("click", function(event) {
+
+});
+
+
+$(".button1").on("click", function(event) {
+
+});
+
+
+$(".button1").on("click", function(event) {
+
+});
+
+
+$(".button1").on("click", function(event) {
+
+});
+
+// generating a number between 19 and 120
+function randomNumberGenerator
+Math.floor(Math.random() * 6) + 1
