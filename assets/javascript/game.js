793cd75c49d06660c17c6af2d30673652da596f3
Allison Snipes <ajsnipes@usfca.edu>
2019-03-27 14:54:29 -0700

    working on js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,47 @@
-'use strict'
+'use strict'; //always place this at the start
 
 $(document).ready(function() {
-  let total,
-    wins,
-    losses = 0;
-  let button1 =;
-  let button1 =;
-  let button1 =;
-  let button1 =;
-  let randomNumber = randomNumber;
+  gameStart();
+  randomNumberGenerator();
+  let total = 0;
+  let wins = 0;
+  let losses = 0;
+  let randomNumber = Math.floor(Math.random() * 102 + 18);
 });
 
+//function to generate random number at start of game
 
-// special button functions to generate random number
-$(".button1").on("click", function(event) {
+function gameStart() {
+  $(".randomNumberPop").html(`
+  <legend>
+    ${randomNumber}
+  </legend>`
 
-});
+  );
+}
+// special button functions to generate random number for each button pressed
+function randomNumberGenerator() {
+  event.preventDefault();
+  //event listening for user clicking the crystal button
+  $(".button1").on("click", event => {
+
+  });
 
 
-$(".button1").on("click", function(event) {
+  $(".button1").on("click", function(event) {
 
-});
-
-
-$(".button1").on("click", function(event) {
-
-});
+  });
 
 
-$(".button1").on("click", function(event) {
+  $(".button1").on("click", function(event) {
 
-});
+  });
 
+
+  $(".button1").on("click", function(event) {
+
+  });
+}
 // generating a number between 19 and 120
 function randomNumberGenerator
 Math.floor(Math.random() * 6) + 1
