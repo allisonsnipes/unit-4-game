98013ceb1c49b35446fa4db124c718124402e445
Allison Snipes <ajsnipes@usfca.edu>
2019-03-27 19:09:32 -0700

    working on js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,33 @@
-'use strict'; //always place this at the start
+ //always place this at the start
+ 'use strict';
 
+ //document ready function
 $(document).ready(function() {
   gameStart();
   randomNumberGenerator();
-  let total = 0;
-  let wins = 0;
-  let losses = 0;
-  let randomNumber = Math.floor(Math.random() * 102 + 18);
+  statsCalc();
+
 });
 
-//function to generate random number at start of game
+//global variables defined here
+let total = 0,
+    wins = 0,
+    losses = 0,
+    chances = 10;
 
+//random number at start
+function startMath() {
+  let randomNumber = Math.floor(Math.random() * 101) + 19;
+}
+
+
+//function to display generated random number (between 19 and 120)
 function gameStart() {
   $(".randomNumberPop").html(`
-  <legend>
-    ${randomNumber}
-  </legend>`
- );
+    <legend>
+      ${randomNumber}
+    </legend>
+  `);
 }
 // special button functions to generate random number for each button pressed
 function randomNumberGenerator() {
@@ -41,6 +52,18 @@ function randomNumberGenerator() {
 
   });
 }
-// generating a number between 19 and 120
-function randomNumberGenerator
-Math.floor(Math.random() * 6) + 1
+
+
+
+//game logic if userNumber === random number
+if (userNumber === randomNumber) {
+  wins++;
+} else { //keep increasing buttons
+  chances--;
+}
+
+
+function statsCalc() {
+  const percentage = ((wins/10) * 100);
+  $(".stats").text(`${percentage} %`)
+}
