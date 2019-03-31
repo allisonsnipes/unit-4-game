9eb76affe3a2a6a17868ad2b91660c7cf74c0418
Allison Snipes <ajsnipes@usfca.edu>
2019-03-31 14:30:40 -0700

    game works but not message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   startMath();
   generateRandomCrystalNum();
   addingAllCrystals();
-  // restartGame();
+  //restartGame();
 });
 
 //global variables defined
@@ -20,7 +20,7 @@ let wins = 0,
 //generate a random number on webpage load (between 19 and 120)
 function startMath() {
   //math for generating random number
-  randomNumber = Math.floor(Math.random() * 101) + 19;
+  randomNumber = Math.floor(Math.random() * 102) + 18;
   console.log(randomNumber);
   //display random number
   $(".randomNumber").text(`${randomNumber}`);
@@ -42,36 +42,37 @@ function addingAllCrystals() {
       sum = crystalRanNum;
       $(".totalScore").text(`${sum}`); //displays the crystal sum on button click
     } else {
-      sum = sum + crystalRanNum
+      sum = sum + crystalRanNum;
       $(".totalScore").text(`${sum}`);//displays the crystal sum on button click
+    }
    });
   }
 
-// //game logic if userNumber === random number
-// if (crystalRanNum === randomNumber) {//function if guess is on point
-//   wins++; //increase win number
-//   $(".wins").text(`${wins}`); //displays current # of wins
-//   $(".losses").text(`${losses}`); //displays current # of losses
-//   restartGame(); //restart game
+//game logic if userNumber === random number
+if (sum  === randomNumber) {//function if guess is on point
+  console.log("random number works");
+  wins++; //increase win number
+  $(".wins").text(`${wins}`); //displays current # of wins
+  $(".losses").text(`${losses}`); //displays current # of losses
+  restartGame(); //restart game}
+} else if (sum < randomNumber) {//function if the guess is too low
+  generateRandomCrystalNum(); //generate another random crystal number again
+  addingAllCrystals(); //add upthe crystals again
+  $(".wins").text(`${wins}`);
+  $(".losses").text(`${losses}`);
+} else {
+  //logic if user guesses too high
+  prompt("Sorry chap, you guessed too high. Let's play again.");
+  losses++; //increase losses
+  $(".wins").text(`${wins}`);
+  $(".losses").text(`${losses}`);
+  restartGame(); //restart game
+}
 
-// } else if (crystalRanNum < randomNumber) {//function if the guess is too low
-//   generateRandomCrystalNum(); //generate another random crystal number again
-//   addingAllCrystals(); //add upthe crystals again
-//   $(".wins").text(`${wins}`);
-//   $(".losses").text(`${losses}`);
-// } else {
-//   //logic if user guesses too high
-//   prompt("Sorry chap, you guessed too high. Let's play again.");
-//   losses++; //increase losses
-//   $(".wins").text(`${wins}`);
-//   $(".losses").text(`${losses}`);
-//   restartGame(); //restart game
-// }
-
-// //function to restart game every win/loss
-// function restartGame() {
-//   wins = 0;
-//   losses = 0;
-//   startMath();
-//   generateRandomCrystalNum();
-// }
+//function to restart game every win/loss
+function restartGame() {
+  wins = 0;
+  losses = 0;
+  startMath();
+  generateRandomCrystalNum();
+}
